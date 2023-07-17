9283967de63ca2cb26f560086502b685ea43e33b
Pedro <pedro@pepicrft.me>
2023-07-17 15:15:23 +0200

    Add frameworks

--- lib/glossia_web/controllers/home_html.ex
+++ lib/glossia_web/controllers/home_html.ex
@@ -3,4 +3,22 @@ defmodule GlossiaWeb.HomeHTML do
   use GlossiaWeb, :marketing_html
 
   embed_templates "home_html/*"
+
+  def frameworks do
+    [
+      %{image: "rails.svg", url: "https://rubyonrails.org", alt: "The Ruby on Rails framework"},
+      %{image: "remix.png", url: "https://remix.run", alt: "The Remix framework"},
+      %{image: "nuxt.svg", url: "https://nuxt.com", alt: "The NuxtJS framework"},
+      %{image: "redwood.svg", url: "https://redwoodjs.com", alt: "The Redwood framework"},
+      %{image: "fresh.svg", url: "https://fresh.deno.dev", alt: "The Fresh framework"},
+      %{image: "next.svg", url: "https://nextjs.org", alt: "The NextJS framework"},
+      %{
+        image: "phoenix.svg",
+        url: "https://www.phoenixframework.org",
+        alt: "The Phoenix framework"
+      },
+      %{image: "astro.svg", url: "https://astro.build", alt: "The Astro framework"},
+      %{image: "laravel.svg", url: "https://laravel.com", alt: "The Laravel framework"}
+    ]
+  end
 end
