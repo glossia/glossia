817587cd954c16dc4ab16c635da1047307e77987
Pedro <pedro@pepicrft.me>
2023-07-17 15:46:24 +0200

    Dismiss some stuff

--- lib/glossia.ex
+++ lib/glossia.ex
@@ -1,5 +1,5 @@
 defmodule Glossia do
-  use Boundary, exports: [Accounts, Accounts.User, Auth, Mailer]
+  use Boundary, exports: [Accounts, Accounts.User, Auth, Mailer, Analytics]
 
   @moduledoc """
   Glossia keeps the contexts that define your domain
--- lib/glossia/vcs.ex
+++ lib/glossia/vcs.ex
@@ -8,13 +8,13 @@ defmodule Glossia.VCS do
   alias Glossia.Accounts.{Credential, User}
 
   @spec repositories(user :: Glossia.Accounts.User.t()) :: [map()]
-  def repositories(user) do
+  def repositories(_user) do
     []
   end
 
   @spec github_credential(user :: User.t()) :: Credential.t() | nil
   defp github_credential(user) do
-    user = user |> Repo.preload(:credentials)
+    _user = user |> Repo.preload(:credentials)
     nil
   end
 end
