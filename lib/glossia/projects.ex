147113c044ff96916360a5abb762565fcfa24114
Pedro <pedro@pepicrft.me>
2023-07-20 18:48:22 +0200

    Create an interface to create projects

--- lib/glossia/projects.ex
+++ lib/glossia/projects.ex
@@ -6,7 +6,12 @@ defmodule Glossia.Projects do
   alias Glossia.Repo
   alias Glossia.Projects.Project
 
+  @doc """
+  Creates a new project with the given attributes.
+  """
+  @spec create_project(opts :: Project.changeset_attrs()) ::
+          {:ok, Project.t()} | {:error, Ecto.Changeset.t()}
   def create_project(opts) do
-    # Project.changeset(%Project{}, %{}) |> Repo.insert()
+    Project.changeset(%Project{}, opts) |> Repo.insert()
   end
 end
