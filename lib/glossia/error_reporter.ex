13613f41f07a41d9813122df927068790f97df2d
Pedro <pedro@pepicrft.me>
2023-07-25 13:42:33 +0200

    Fix key

--- lib/glossia/error_reporter.ex
+++ lib/glossia/error_reporter.ex
@@ -1,5 +1,5 @@
 defmodule Glossia.ErrorReporter do
   def handle_event([:oban, :job, :exception], _measure, meta, _) do
-    Appsignal.set_error(meta.error, meta.stack)
+    Appsignal.set_error(meta.error, meta.stacktrace)
   end
 end
