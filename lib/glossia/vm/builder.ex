8095126d878cc926d57c6edc11d259109ed18b1c
Pedro <pedro@pepicrft.me>
2023-07-25 17:14:50 +0200

    Some fixes

--- lib/glossia/vm/builder.ex
+++ lib/glossia/vm/builder.ex
@@ -17,13 +17,17 @@ defmodule Glossia.VM.Builder do
           {:ok, String.t()}
   def run(command: command, env: env, status_update_cb: status_update_cb) do
     if Application.get_env(:glossia, :env) == :prod do
-      run_in_google_cloud_build(command: command, env: env, status_update_cb: status_update_cb)
+      run_using_google_cloud_build(command: command, env: env, status_update_cb: status_update_cb)
     else
-      run_locally(command: command, env: env, status_update_cb: status_update_cb)
+      run_using_docker(command: command, env: env, status_update_cb: status_update_cb)
     end
   end
 
-  defp run_in_google_cloud_build(command: command, env: env, status_update_cb: status_update_cb) do
+  defp run_using_google_cloud_build(
+         command: command,
+         env: env,
+         status_update_cb: status_update_cb
+       ) do
     # https://cloud.google.com/build/docs/api/reference/rest/v1/projects.builds/create
     # https://github.com/googleapis/elixir-google-api/blob/main/clients/cloud_build/lib/google_api/cloud_build/v1/api/projects.ex#L213
     project_id = Application.get_env(:glossia, :secrets)[:google_cloud_project_id]
@@ -38,11 +42,11 @@ defmodule Glossia.VM.Builder do
           timeout: "#{@timeout}s",
           steps: [
             %GoogleApi.CloudBuild.V1.Model.BuildStep{
-              name: docker_image(),
-              args: deno_arguments(command: command, env: env),
+              name: get_docker_image(),
+              args: get_deno_args(command: command, env: env),
               env:
                 env
-                |> Enum.into(docker_env_variables())
+                |> Enum.into(get_docker_env_variables())
                 |> Enum.reduce([], fn {k, v}, acc -> ["#{k}=#{v}" | acc] end)
             }
           ]
@@ -61,11 +65,11 @@ defmodule Glossia.VM.Builder do
     )
   end
 
-  def monitor_google_cloud_build(
-        build_id: build_id,
-        project_id: project_id,
-        status_update_cb: status_update_cb
-      ) do
+  defp monitor_google_cloud_build(
+         build_id: build_id,
+         project_id: project_id,
+         status_update_cb: status_update_cb
+       ) do
     :timer.sleep(2000)
     {:ok, token} = Goth.fetch(Glossia.Goth)
 
@@ -89,9 +93,9 @@ defmodule Glossia.VM.Builder do
     end
   end
 
-  defp run_locally(command: command, env: env, status_update_cb: status_update_cb) do
+  defp run_using_docker(command: command, env: env, status_update_cb: status_update_cb) do
     docker_env_flags =
-      docker_env_variables()
+      get_docker_env_variables()
       |> Enum.into(env)
       |> Enum.reduce([], fn {k, v}, acc ->
         ["--env", "#{Atom.to_string(k)}=#{v}" | acc]
@@ -100,12 +104,12 @@ defmodule Glossia.VM.Builder do
     arguments =
       ["docker", "run"] ++
         ["--init"] ++
-        ["--volume", builder_directory() <> ":" <> "/builder"] ++
+        ["--volume", get_builder_directory() <> ":" <> "/builder"] ++
         ["--workdir", "/builder"] ++
         ["--publish", "4000:4000"] ++
         docker_env_flags ++
-        [docker_image()] ++
-        deno_arguments(command: command, env: env)
+        [get_docker_image()] ++
+        get_deno_args(command: command, env: env)
 
     task =
       Task.async(fn ->
@@ -126,8 +130,8 @@ defmodule Glossia.VM.Builder do
     end)
   end
 
-  @spec docker_env_variables() :: map()
-  defp docker_env_variables() do
+  @spec get_docker_env_variables() :: map()
+  defp get_docker_env_variables() do
     %{
       GLOSSIA_URL: Application.get_env(:glossia, :url),
       GLOSSIA_APP_SIGNAL_API_KEY:
@@ -135,11 +139,8 @@ defmodule Glossia.VM.Builder do
     }
   end
 
-  @doc """
-  It returns the arguments to pass to the `deno` executable.
-  """
-  @spec deno_arguments(attrs :: [command: String.t(), env: map()]) :: [String.t()]
-  defp deno_arguments(command: command, env: env) do
+  @spec get_deno_args(attrs :: [command: String.t(), env: map()]) :: [String.t()]
+  defp get_deno_args(command: command, env: env) do
     path =
       if Application.get_env(:glossia, :env) == :prod do
         Application.get_env(:glossia, :url) <> "/builder/index.ts"
@@ -147,25 +148,38 @@ defmodule Glossia.VM.Builder do
         "./index.ts"
       end
 
-    ["run", "--allow-net", "--allow-env=#{Enum.join(deno_allow_env_variables(env), ",")}"] ++
-      [path, command]
+    deno_allow_env_flags =
+      (Enum.reduce(env, [], fn {k, _}, acc ->
+         [Atom.to_string(k) | acc]
+       end) ++ ["GLOSSIA_URL", "GLOSSIA_APP_SIGNAL_API_KEY"])
+      |> Enum.join(",")
+
+    ["run", "--allow-net", "--allow-env=#{deno_allow_env_flags}", path, command]
   end
 
-  def docker_image() do
+  @doc """
+  It returns the Docker image to use for the builder.
+  """
+  @spec get_docker_image() :: String.t()
+  def get_docker_image() do
     "denoland/deno:" <> Application.get_env(:glossia, :versions)[:deno]
   end
 
-  def deno_allow_env_variables(env) do
-    Enum.reduce(env, [], fn {k, _}, acc ->
-      [Atom.to_string(k) | acc]
-    end) ++ ["GLOSSIA_URL", "GLOSSIA_APP_SIGNAL_API_KEY"]
-  end
-
-  def builder_directory() do
+  @doc """
+  It returns the directory where the builder executable is located.
+  This is necessary when running local builds using Docker to mount the
+  builder directory and run the executable.
+  """
+  @spec get_builder_directory() :: String.t()
+  def get_builder_directory() do
     app_dir = Application.app_dir(:glossia)
     Path.join([app_dir, "priv", "static", "builder"])
   end
 
+  @doc """
+  It returns true if Docker is available in the system.
+  """
+  @spec docker_available?() :: boolean()
   def docker_available?() do
     case Exile.stream(["/usr/bin/env", "which", "docker"]) |> Enum.to_list() do
       [_, {:exit, {:status, 0}}] -> true
