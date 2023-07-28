cbacd5bedb9c2c965af7c067e96b98391dcbaf77
Pedro <pedro@pepicrft.me>
2023-07-28 16:40:48 +0200

    Parse the configuration files

--- priv/static/builder/configuration.ts
+++ priv/static/builder/configuration.ts
@@ -1,13 +1,61 @@
-import { z } from "https://deno.land/x/zod@v3.31.4/mod.ts";
-import { jsonSchemaToZod } from "npm:json-schema-to-zod@1.1.1";
+import Ajv from "https://esm.sh/ajv@8.6.1";
+import { expandGlob } from "https://deno.land/std@0.196.0/fs/mod.ts";
+import { parse } from "https://deno.land/std@0.195.0/jsonc/mod.ts";
 
 import configurationV1JSONSchema from "../schemas/configuration/v1.json" assert {
   type: "json",
 };
 
-export const configurationSchema = jsonSchemaToZod(
-  configurationV1JSONSchema as any,
-);
+type Configuration = {
+  path: string;
+  languages: {
+    source: string;
+    target: string[];
+  };
+  files: string[];
+};
 
-export async function loadConfiguration({ from }: { from: string }) {
+export async function loadConfigurations(
+  { root }: { root: string },
+): Promise<Configuration[]> {
+  const configurationFilePaths: string[] = [];
+  for await (
+    const configurationFilePath of expandGlob("**/glossia.jsonc", {
+      root: root,
+    })
+  ) {
+    configurationFilePaths.push(configurationFilePath.path);
+  }
+  const ajv = new Ajv();
+  const validate = ajv.compile(configurationV1JSONSchema);
+
+  const configurations: Configuration[] = await Promise.all(
+    configurationFilePaths.map(async (configurationFilePath) => {
+      console.log(`Reading configuration ${configurationFilePath}`);
+      const configurationFile = parse(
+        await Deno.readTextFile(configurationFilePath),
+      );
+      console.log(`Validating configuration file ${configurationFilePath}`);
+      if (!validate(configurationFile)) {
+        let error = null;
+        if (validate.errors) {
+          error = validate.errors![validate.errors!.length - 1];
+        }
+        console.error(
+          `The validation of the configuration file at path ${configurationFilePath} failed:`,
+          error,
+        );
+      }
+      return {
+        ...(configurationFile as any),
+        path: configurationFilePath,
+      };
+    }),
+  );
+
+  if (validate.errors) {
+    throw new Error("Invalid configuration");
+  }
+
+  return configurations;
 }
--- priv/static/builder/events/git_push.ts
+++ priv/static/builder/events/git_push.ts
@@ -1,6 +1,9 @@
+import { loadConfigurations } from "../configuration.ts";
 import { cloneGitRepository } from "../git.ts";
 
 export async function processGitPush() {
   const tempDirPath = await Deno.makeTempDir();
   await cloneGitRepository({ into: tempDirPath });
+  const configurationFiles = await loadConfigurations({ root: tempDirPath });
+  console.log(configurationFiles);
 }
