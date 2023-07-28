23bd4078f07828a4a450dee914321a33d28fa2cb
Pedro <pedro@pepicrft.me>
2023-07-28 13:51:58 +0200

    Fix event

--- priv/static/builder/index.ts
+++ priv/static/builder/index.ts
@@ -15,7 +15,7 @@ if (appSignalAPIKey) {
 
 try {
   outputHeadingTableWithContext();
-  if (getEvent() === "push") {
+  if (getEvent() === "git_push") {
     // https://github.blog/2020-12-21-get-up-to-speed-with-partial-clone-and-shallow-clone/
     const remoteURL =
       `https://${getGitAccessToken()}@${getGitRepositoryVersionControl()}.com/${getGitRepositoryId()}.git`;
