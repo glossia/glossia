e46989ef170d41e1336cd3879fc17644581280b0
Pedro <pedro@pepicrft.me>
2023-07-30 21:30:36 +0200

    Generate the translation payload

---
+++ priv/static/builder/translation.ts
@@ -0,0 +1,162 @@
+import Ajv, { ValidateFunction } from "https://esm.sh/ajv@8.6.1";
+import { expandGlob } from "https://deno.land/std@0.196.0/fs/mod.ts";
+import { parse } from "https://deno.land/std@0.195.0/jsonc/mod.ts";
+import { dirname, relative } from "https://deno.land/std@0.196.0/path/posix.ts";
+
+type TranslationModuleLanguages = {
+  source: string;
+  target: string[];
+};
+
+type TranslationModule = {
+  languages: TranslationModuleLanguages;
+  files: string[];
+};
+
+type TranslationPayload = TranslationModule[];
+
+type GenerateTranslationPayloadOptions = { root: string };
+
+/**
+ * It looks up the configuration files and generates the translation payload.
+ *
+ * @param options { GenerateTranslationPayloadOptions} The options to generate the translation payload
+ * @returns {Promise<TranslationPayload>} The translation payload
+ */
+export async function generateTranslationPayload(
+  options: GenerateTranslationPayloadOptions,
+): Promise<TranslationPayload> {
+  console.info("Loading configuration", { fromDirectory: options.root });
+  const validate = await getConfigurationValidate();
+  const configurationFilePaths: string[] = await loadConfigurationFilePaths(
+    options.root,
+  );
+  const translationModules: TranslationModule[] =
+    await loadConfigurationsResolvingFiles(
+      configurationFilePaths,
+      validate,
+    );
+  if (validate.errors) {
+    throw new Error("Invalid configuration files found");
+  }
+  return translationModules;
+}
+
+/**
+ * It returns a function that validates the configuration.
+ * Documentation: https://ajv.js.org/guide/managing-schemas.html
+ *
+ * @returns {Promise<(data: any) => boolean>} A function that validates the configuration
+ */
+export async function getConfigurationValidate() {
+  const configurationSchema = await import("../schemas/configuration.json", {
+    assert: { type: "json" },
+  });
+  const languageSchema = await import("../schemas/language.json", {
+    assert: { type: "json" },
+  });
+
+  const ajv = new Ajv({
+    strict: true,
+    strictSchema: true,
+    strictTypes: true,
+    strictRequired: true,
+    validateSchema: true,
+    strictTuples: false,
+    allErrors: true,
+  });
+
+  ajv.addSchema(languageSchema.default, languageSchema.default["$id"]);
+
+  return ajv.compile(configurationSchema.default);
+}
+
+async function loadConfigurationsResolvingFiles(
+  configurationFilePaths: string[],
+  validate: ValidateFunction<unknown>,
+): Promise<TranslationModule[]> {
+  const translationModules = await Promise.all(
+    configurationFilePaths.map(async (configurationFilePath) => {
+      console.info(`Reading configuration ${configurationFilePath}`);
+      const configurationFile = parse(
+        await Deno.readTextFile(configurationFilePath),
+      );
+      console.info(
+        `Validating configuration file ${configurationFilePath}`,
+        configurationFile,
+      );
+      const isValid = validate(configurationFile);
+      console.info(`The validity of the configuration file is ${isValid}`);
+      if (!isValid) {
+        let error = null;
+        if (validate.errors) {
+          error = validate.errors![validate.errors!.length - 1];
+        }
+        console.error(
+          `The validation of the configuration file at path ${configurationFilePath} failed:`,
+          error,
+        );
+        return null;
+      } else {
+        return {
+          languages:
+            // deno-lint-ignore ban-ts-comment
+            // @ts-ignore
+            configurationFile["languages"] as TranslationModuleLanguages,
+          files: await resolveConfigurationFiles(
+            // deno-lint-ignore ban-ts-comment
+            // @ts-ignore
+            configurationFile["files"] as string[],
+            {
+              root: dirname(configurationFilePath),
+              languages:
+                // deno-lint-ignore ban-ts-comment
+                // @ts-ignore
+                configurationFile["languages"] as TranslationModuleLanguages,
+            },
+          ),
+        };
+      }
+    }),
+  );
+
+  return translationModules.filter((translationModule) =>
+    translationModule !== undefined
+  ) as TranslationPayload;
+}
+
+async function resolveConfigurationFiles(
+  files: string,
+  { root, languages }: {
+    root: string;
+    languages: { source: string; target: string[] };
+  },
+) {
+  const sourceAndTargetLanguages = [languages.source, ...languages.target];
+  return (await Promise.all(
+    sourceAndTargetLanguages.map(async (language) => {
+      const languageGlob = files.replace("{language}", language);
+      const languageFiles: string[] = [];
+      for await (
+        const languageFilePath of expandGlob(languageGlob, {
+          root: root,
+        })
+      ) {
+        languageFiles.push(relative(root, languageFilePath.path));
+      }
+      return languageFiles;
+    }),
+  )).flat();
+}
+
+async function loadConfigurationFilePaths(root: string) {
+  const configurationFilePaths: string[] = [];
+  for await (
+    const configurationFilePath of expandGlob("**/glossia.jsonc", {
+      root: root,
+    })
+  ) {
+    configurationFilePaths.push(configurationFilePath.path);
+  }
+  return configurationFilePaths;
+}
