5a0db48790f824444ec05ef442a31c41b3befc9d
Pedro <pedro@pepicrft.me>
2023-08-01 09:10:49 +0200

    Update ajv

--- priv/static/builder/vcs.ts
+++ priv/static/builder/vcs.ts
@@ -1,4 +1,4 @@
-import Ajv, { ValidateFunction } from "https://esm.sh/ajv@8.6.1";
+import Ajv, { ValidateFunction } from "https://esm.sh/ajv@~8.12.0";
 import { expandGlob } from "https://deno.land/std@0.196.0/fs/mod.ts";
 import { parse } from "https://deno.land/std@0.195.0/jsonc/mod.ts";
 import { dirname, relative } from "https://deno.land/std@0.196.0/path/posix.ts";
