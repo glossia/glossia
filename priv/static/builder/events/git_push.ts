8d40189fe8a2f1514118b0b0c96ca1ed7cc47cdc
Pedro <pedro@pepicrft.me>
2023-08-14 18:13:21 +0200

    Trigger a new build

--- priv/static/builder/events/git_push.ts
+++ priv/static/builder/events/git_push.ts
@@ -1,11 +1,13 @@
 import { generateTranslationPayload } from "../utils/vcs.ts";
 import { cloneGitRepository } from "../utils/git.ts";
+import { loadConfigurationManifests } from "../utils/vcs/configuration_loader.ts";
 
 export default async function gitPush() {
   const tempDirPath = await Deno.makeTempDir();
   await cloneGitRepository({ root: tempDirPath });
-  const translationPayload = await generateTranslationPayload({
+  const configurationManifests = await loadConfigurationManifests({
     root: tempDirPath,
   });
-  console.log(translationPayload);
+
+  console.log(configurationManifests);
 }
