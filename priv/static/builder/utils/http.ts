15e230f628d0593fa61703024357d68528e553dc
Pedro <pedro@pepicrft.me>
2023-08-21 17:41:39 +0200

    Add more verbose logs

--- priv/static/builder/utils/http.ts
+++ priv/static/builder/utils/http.ts
@@ -8,7 +8,7 @@ export async function glossiaFetch<T>(
 ): Promise<T> {
   const absoluteURL = new URL(path, getURL());
   console.info("Sending requests to Glossia", absoluteURL.toString());
-  const jsonResponse = await fetch(absoluteURL.toString(), {
+  const response = await fetch(absoluteURL.toString(), {
     ...init,
     headers: {
       ...init?.headers,
@@ -16,18 +16,20 @@ export async function glossiaFetch<T>(
       "authorization": `Bearer ${getAccessToken()}`,
     },
   });
+  const text = await response.text();
+
   let jsonData;
   try {
-    jsonData = await jsonResponse.json();
+    jsonData = JSON.parse(text);
   } catch (error) {
     console.error(
       "Error parsing JSON response from Glossia",
-      jsonResponse.text(),
+      text,
     );
     throw error;
   }
   console.info("Glossia responded", {
-    status: jsonResponse.status,
+    status: response.status,
     body: jsonData,
   });
   return jsonData;
