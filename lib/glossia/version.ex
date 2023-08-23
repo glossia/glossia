921550931298eef5d56545e653793a9c7a6eeac1
Pedro <pedro@pepicrft.me>
2023-08-23 08:11:19 +0200

    Add utilities to selectively compile based on the plan

--- lib/glossia/version.ex
+++ lib/glossia/version.ex
@@ -1,8 +1,14 @@
 defmodule Glossia.Version do
   use Boundary
 
+  # Module
+
   @current File.read!("priv/version.txt")
   @external_resource "priv/version.txt"
 
+  @doc """
+  It returns the current version of Glossia.
+  """
+  @spec current_version() :: String.t()
   def current_version, do: @current
 end
