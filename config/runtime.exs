d4a1ae2cd95948312df8df37fcb8a90f575bec07
Pedro <pedro@pepicrft.me>
2023-09-07 18:25:05 +0200

    Fix the host

--- config/runtime.exs
+++ config/runtime.exs
@@ -51,7 +51,7 @@ if config_env() == :prod do
       You can generate one by calling: mix phx.gen.secret
       """
 
-  host = System.get_env("PHX_HOST") || "glossia.ai"
+  host = System.get_env("PHX_HOST") || "https://glossia.ai"
   port = String.to_integer(System.get_env("PORT") || "4000")
 
   config :glossia, Glossia.Application.Endpoint,
