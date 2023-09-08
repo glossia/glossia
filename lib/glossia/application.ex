fd1850dc556ca9be85866479e988faab6bdf96b7
Pedro <pedro@pepicrft.me>
2023-09-08 15:51:49 +0200

    Implement hasher

--- lib/glossia/application.ex
+++ lib/glossia/application.ex
@@ -43,7 +43,8 @@ defmodule Glossia.Application do
         # Start a worker by calling: Glossia.Worker.start_link(arg)
         # {Glossia.Worker, arg}
         {Oban, Application.fetch_env!(:glossia, Oban)},
-        {PlugAttack.Storage.Ets, name: Glossia.Web.Plugs.AttackPlug.Storage, clean_period: 60_000},
+        {PlugAttack.Storage.Ets,
+         name: Glossia.Web.Plugs.AttackPlug.Storage, clean_period: 60_000},
         {Task.Supervisor, name: Glossia.TaskSupervisor}
       ] ++ google_cloud_children()
 
