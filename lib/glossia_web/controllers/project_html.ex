74b0258e67dd2d9761c324f9bb22ecd98eb901d7
Pedro <pedro@pepicrft.me>
2023-11-02 10:00:34 +0100

    Add some content to the admin

--- lib/glossia_web/controllers/project_html.ex
+++ lib/glossia_web/controllers/project_html.ex
@@ -1,11 +1,5 @@
 defmodule GlossiaWeb.Controllers.ProjectHTML do
   use GlossiaWeb.Helpers.App, :html
 
-  def show(assigns) do
-    ~H"""
-    <div>
-      <.link href={~p"/auth/logout"} method="delete">Sign out</.link>
-    </div>
-    """
-  end
+  embed_templates "project_html/*"
 end
