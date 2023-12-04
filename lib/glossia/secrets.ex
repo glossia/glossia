5e982cddd44ba8547f3c94eb36d741757de5a541
Pedro <pedro@pepicrft.me>
2023-12-04 17:28:11 +0100

    Fix file location

--- lib/glossia/secrets.ex
+++ lib/glossia/secrets.ex
@@ -1,5 +1,8 @@
 defmodule Glossia.Secrets do
   @secrets_env_key :secrets
+  @base_directory "priv/secrets"
+  @secrets_file_location @base_directory <> "/secrets.yml.enc"
+  @key_file_location @base_directory <> "/master.key"
 
   def get_in(keys) when is_list(keys) do
     secrets() |> get_in(keys)
@@ -13,8 +16,7 @@ defmodule Glossia.Secrets do
     secrets =
       if System.get_env("MASTER_KEY") do
         {:ok, cwd} = File.cwd()
-        path = Path.join([cwd, "../priv/secrets/secrets.yml.enc"])
-        EncryptedSecrets.read!(System.fetch_env!("MASTER_KEY"), path)
+        EncryptedSecrets.read!(System.fetch_env!("MASTER_KEY"), @key_file_location)
       else
         EncryptedSecrets.read!()
       end
