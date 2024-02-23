e6168fbb318b26650eefac342f3db5b6d3b5b188
Pedro <pedro@pepicrft.me>
2024-02-23 16:53:41 +0100

    Clean the previous content

--- marketing/src/pages/rss.xml.js
+++ marketing/src/pages/rss.xml.js
@@ -1,6 +1,6 @@
 import rss from "@astrojs/rss";
 import { getCollection } from "astro:content";
-import { siteDescription, siteTitle } from "../consts";
+import { siteDescription, siteTitle } from "../constants";
 
 export async function GET(context) {
   const posts = await getCollection("blog");
