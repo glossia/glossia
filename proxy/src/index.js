24ebd2407fc40875eb5c4a70c1c125aae553c203
Pedro <pedro@pepicrft.me>
2024-02-23 18:15:15 +0100

    Update the proxy

--- proxy/src/index.js
+++ proxy/src/index.js
@@ -1,11 +1,16 @@
 export default {
   async fetch(request) {
     const path = new URL(request.url).pathname;
+    const isAuthorized = request.headers.has("Authorization");
+
     if (path.startsWith("/docs")) {
       const baseURL = `https://glossia-documentation.pages.dev`;
       return fetch(baseURL + path, request);
     } else if (path.startsWith("/handbook")) {
       const baseURL = `https://glossia-handbook.pages.dev`;
+      return fetch(baseURL + path, request);
+    } else if (!isAuthorized || path == "" || path == "/") {
+      const baseURL = `https://glossia-marketing.pages.dev`;
       return fetch(baseURL + path, request);
     } else {
       const baseURL = `https://glossia.fly.dev`;
