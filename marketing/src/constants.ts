1f7c6a5007181a5abcf0e97825d89c02196a3b63
Pedro <pedro@pepicrft.me>
2024-02-27 13:15:09 +0100

    Add SEO to the index page

--- marketing/src/constants.ts
+++ marketing/src/constants.ts
@@ -1,2 +1,10 @@
-export const siteTitle = "Glossia";
-export const siteDescription = "An OSS Localization OS";
+export const siteTitle = "Glossia - Open Source Localization Operative System";
+export const siteDescription =
+  "Glossia is an open source localization operative system that leverages the power of LLMs to incrementally localize content from content sources like GitHub, GitLab, Shopify Stores, Apps, and Themes, Figma designs, and more.";
+export function siteURL() {
+  if (import.meta.env.PROD) {
+    return new URL(import.meta.env.SITE);
+  } else {
+    return new URL("http://localhost:4321/");
+  }
+}
