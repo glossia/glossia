63d6e551deb5856cd005e62c0b64e6c11beec7e5
Pedro <pedro@pepicrft.me>
2024-02-27 18:17:10 +0100

    Add SEO metadata

--- marketing/src/constants.ts
+++ marketing/src/constants.ts
@@ -1,6 +1,7 @@
 export const siteTitle = "Glossia - Open Source Localization Operative System";
 export const siteDescription =
   "Glossia is an open source localization operative system that leverages the power of LLMs to incrementally localize content from content sources like GitHub, GitLab, Shopify Stores, Apps, and Themes, Figma designs, and more.";
+
 export function siteURL() {
   if (import.meta.env.PROD) {
     return new URL(import.meta.env.SITE);
