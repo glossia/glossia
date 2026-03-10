9e2417670f571c59652f461979ed5cbc5eab6165
Pedro Piñera <pedro@pepicrft.me>
2026-03-10 12:26:33 +0100

    chore: ignore and untrack agent build artifacts

--- agent/deps/burrito/_dummy_plugin.zig
+++
@@ -1,4 +0,0 @@
-pub fn burrito_plugin_entry(install_dir: []const u8, program_manifest_json: []const u8) void {
-    _ = install_dir;
-    _ = program_manifest_json;
-}
--- agent/deps/weather/lib/weather/test_response/bad_request.ex
+++
@@ -1,6 +0,0 @@
-defmodule Weather.TestResponse.BadRequest do
-  @moduledoc false
-
-  @spec response() :: map()
-  def response, do: %{"cod" => 400, "message" => "wrong latitude"}
-end
